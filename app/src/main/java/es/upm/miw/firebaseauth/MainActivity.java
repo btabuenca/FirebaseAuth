d6426912a6bb439323a8a42722a34e37f70342b9
btabuenca <bernardo.tabuenca@gmail.com>
2021-10-26 09:38:20 +0200

    Improved interface

--- app/src/main/java/es/upm/miw/firebaseauth/MainActivity.java
+++ app/src/main/java/es/upm/miw/firebaseauth/MainActivity.java
@@ -1,19 +1,3 @@
-/**
- * Copyright 2016 Google Inc. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package es.upm.miw.firebaseauth;
 
 import android.os.Bundle;
@@ -154,7 +138,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         // Status text
         if (isSignedIn) {
-            uidView.setText(getString(R.string.id_fmt, user.getDisplayName()));
+            uidView.setText(R.string.signed_in);
             emailView.setText(getString(R.string.email_fmt, user.getEmail()));
             Log.i(LOG_TAG, "signedIn: " + getString(R.string.id_fmt, user.getDisplayName()));
         } else {
